33054aff9338a5f50b8b6936b7b3e97d8fee0a16
Prashant-kumar-aryan <prashantkr2698@gmail.com>
2024-01-24 12:25:50 +0530

    Added Created at

--- src/Components/Task.jsx
+++ src/Components/Task.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 
 const Task = ({ title, description, deleteTask, index }) => {
+
+  var currentdate = new Date();
+  var datetime = "Created At: " + currentdate.getDay() + "/" + currentdate.getMonth() 
++ "/" + currentdate.getFullYear() + "  " 
++ currentdate.getHours() + ":" 
++ currentdate.getMinutes() + ":" + currentdate.getSeconds();
+
   return (
     <div className="task">
       <div>
         <p>{title}</p>
         <span>{description}</span>
       </div>
-
+      <div className="end">
       <button onClick={() => deleteTask(index)}>-</button>
+      <p className="time">{datetime}</p>
+      </div>
+  
     </div>
   );
 };
